54940961157c02e107a98e347b3cf1b6efa4cab7
ByJfMarie <jf.marie@outlook.fr>
2021-09-22 11:50:16 +0200

    second commit

---
+++ src/components/Accueil.vue
@@ -0,0 +1,17 @@
+<template lang="fr">
+    <div>
+        <h1 class="text-red-600">Test</h1>
+        
+        <h1></h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Accueil',
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="css">
+</style>
---
+++ src/components/foodCard.vue
@@ -0,0 +1,17 @@
+<template lang="fr">
+    <div>
+        
+    </div>
+
+</template>
+<script>
+export default {
+  name: "foodCard",
+  props: {},
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="css">
+</style>
