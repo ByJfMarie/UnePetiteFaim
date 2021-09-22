f8da7752ccb415f9d65b199e402615d1cf314e65
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2021-09-22 15:42:13 +0200

    dqSDQSD

---
+++ src/components/IngredientCard.vue
@@ -0,0 +1,16 @@
+<template lang="fr">
+    <div class="ingredient flex flex-col justify-center items-center m-2 p-2 bg-green-50 rounded-xl shadow-sm">
+        <div class="img h-8 w-8 bg-blue-600 rounded-full"></div>
+        <i>\ud83d\uddb1\ufe0f</i>
+        <div class="ingredient-description text-sm">Ingrédient</div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'IngredientCard',
+
+}
+</script>
+<style lang="">
+    
+</style>
--- src/components/foodCard.vue
+++ src/components/foodCard.vue
@@ -1,15 +1,35 @@
 <template lang="fr">
-    <div>
-        
+    <div class="food-card-container bg-white h-auto w-64 shadow-sm rounded-2xl ml-2 mt-2 p-4 flex flex-col">
+       <div class="header flex items-center mb-3">
+         <div class="img h-16 w-16 bg-red-600 rounded-full"></div>
+         <span class="title ml-2">Le titre ici</span>
+       </div>
+       <h1>Ingrédients</h1>
+       <div class="ingredients-container flex flex-wrap w-full justify-center items-center mt-2 mb-2">
+        <IngredientCard />
+        <IngredientCard />
+        <IngredientCard />
+        <IngredientCard />
+       </div>
+       <div class="footer flex justify-between items-center">
+        <button class="rounded-full pt-1 pb-1 pl-5 pr-5 bg-green-400 text-white">Voir la recette</button>
+        <span class="text-xs">15 mins</span>
+       </div>
+       
     </div>
 
 </template>
 <script>
+import IngredientCard from './IngredientCard.vue';
+  
 export default {
-  name: "foodCard",
+  name: "FoodCard",
   props: {},
   data() {
     return {};
+  },
+  components: {
+    IngredientCard,
   },
 };
 </script>
