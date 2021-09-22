c3cdc0b915423b421da7bf170aa33749f2cc3a95
ByJfMarie <jf.marie@outlook.fr>
2021-09-22 15:43:29 +0200

    dessd

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
+/* eslint-disable */
+
 <template>
-  <div class="searchBar">
-    <i class="fas fa-search"></i>
-    <input type="text" placeholder="Rechercher une recette">
+  <div class="page w-full flex justify-center mt-6 items-center">
+
+    <div class="searchBar bg-white w-1/3 py-2 px-4 rounded-full  shadow-lg mr-4">
+      <i class="fas fa-search mr-6"></i>
+      <input type="text" placeholder="Rechercher une recette" class="focus:outline-none w-5/6" @keyup.enter="search" v-model="searchText" >
+    </div>
+    <button class="filterBar bg-white w-28 py-2 px-4 rounded-full  shadow-lg flex justify-around items-center ml-4">
+      <i class="fas fa-filter"></i>
+      <h1 class="focus:outline-none text-gray-400">Filter</h1>
+    </button>
   </div>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <Accueil />
@@ -9,11 +18,30 @@
 
 <script>
 import Accueil from "./components/Accueil.vue";
+import { searchRecipes, QueryBuilder} from 'marmiton-api';
 
 export default {
   name: "App",
   components: {
     Accueil,
+
+  },
+  data() {
+    return {
+      searchText: "",
+    }
+  },
+  methods: {
+    search() {
+      
+      const query = new QueryBuilder()
+        .withTitleContaining(this.searchText) 
+        .build();
+
+      const recipes = searchRecipes(query);
+
+      console.log(recipes);
+    },
   },
 };
 </script>
