d3f73c6bb39dbaa340d6a8256c8d666b60d2783f
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2021-09-23 22:04:25 +0200

    ptite modif

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
 <template>
   <div class="page w-full flex sm:flex-row flex-col justify-center mt-12 items-center">
 
-    <div class="searchBar bg-white w-5/6 sm:w-2/3 md:w-1/3 py-2 px-4 rounded-full  shadow-lg mr-4">
+    <div class="searchBar bg-white w-5/6 sm:w-2/3 md:w-1/3 py-2 px-4 rounded-full  shadow-lg mr-4 focus-within:shadow-xl">
       <i class="fas fa-search mr-6"></i>
       <input type="text" placeholder="Rechercher une recette" class="focus:outline-none w-4/6 sm:w-5/6" @keyup.enter="search" v-model="searchText" >
     </div>
     <div class="boutons sm:mt-0 mt-4 ml-4 flex">
-    <button class="filterBar bg-white w-28 py-2 px-4 rounded-full  shadow-lg flex justify-around items-center " v-on:click="openFilterPage">
+    <button class="filterBar bg-white w-28 py-2 px-4 rounded-full  shadow-lg flex justify-around items-center focus-within:shadow-xl" v-on:click="openFilterPage">
       <i class="fas fa-filter"></i>
       <h1 class="focus:outline-none text-gray-400">Filter</h1>
     </button>
-    <button class="trieBar bg-white w-28 py-2 px-4 rounded-full  shadow-lg flex justify-around items-center ml-4">
+    <button class="trieBar bg-white w-28 py-2 px-4 rounded-full  shadow-lg flex justify-around items-center ml-4 focus-within:shadow-xl">
       <i class="fas fa-sort-amount-up"></i>
       <h1 class="focus:outline-none text-gray-400">Trier</h1>
     </button>
@@ -55,8 +55,12 @@ export default {
   },
   methods: {
     openFilterPage() {
-      this.FilterPage = true,
-      console.log(this.FilterPage);
+      if (this.FilterPage === false) {
+        this.FilterPage = true;
+      }else {
+        this.FilterPage = false;
+      }
+      
     },
     closeFilterPage() {
       this.FilterPage = false
