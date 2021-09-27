0afcd6415ecba35a774e3ff917a6bed12cd82576
ByJfMarie <jf.marie@outlook.fr>
2021-09-27 08:26:08 +0200

    filter

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-
+/* eslint-disable */
 <template>
   <div class="page w-full flex sm:flex-row flex-col justify-center mt-12 items-center">
 
@@ -22,6 +22,39 @@
   <div class="filtreBlock w-full flex justify-center mt-6" v-if="FilterPage">
     <div class="filterPage bg-white h-64 w-5/12 rounded-xl shadow-lg relative">
       <button v-on:click="closeFilterPage"><i class="fas fa-times absolute top-2 right-4"></i></button>
+      <div class="w-full h-full flex -mt-6 flex-col items-center justify-around">
+
+        <div class="searchBar bg-white w-5/6 sm:w-2/3 md:w-2/3 py-2 px-4 rounded-full h-10 mr-4  border border-gray-100 shadow-md">
+          <i class="far fa-lemon mr-4"></i>
+          <input type="text" placeholder="Ingredient (Ex: Lemon juice,Gin,Mint)" class="focus:outline-none w-4/6 sm:w-5/6 bg-white" @keyup.enter="searchFilter" v-model="searchFilterText" >
+        </div>
+        <div class="w-full flex justify-around items-center">
+          <div class="filtreAlccol">
+            <label for="toggle" class=" text-gray-700">Alcoholic</label>
+            <div class="relative inline-block w-12 ml-2 align-middle select-none transition duration-200 ease-in">
+              <input type="checkbox" name="toggle" id="toggle" class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-2 appearance-none cursor-pointer" v-model="alcoholic"/>
+              <label for="toggle" class="toggle-label block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+            </div>
+          </div>
+          <div class="relative inline-flex" v-if="categories">
+            <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
+            <select class="border border-gray-100 shadow-md rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white focus:outline-none appearance-none" v-model="categoryFiltre">
+              <option>Catégories</option>
+              <option v-for="category in categories" :key="category">{{category.strCategory}}</option>
+            </select>
+          </div>
+          <div class="relative inline-flex">
+            <svg class="w-2 h-2 absolute top-0 right-0 m-4 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 412 232"><path d="M206 171.144L42.678 7.822c-9.763-9.763-25.592-9.763-35.355 0-9.763 9.764-9.763 25.592 0 35.355l181 181c4.88 4.882 11.279 7.323 17.677 7.323s12.796-2.441 17.678-7.322l181-181c9.763-9.764 9.763-25.592 0-35.355-9.763-9.763-25.592-9.763-35.355 0L206 171.144z" fill="#648299" fill-rule="nonzero"/></svg>
+            <select class="border border-gray-100 shadow-md rounded-full text-gray-600 h-10 pl-5 pr-10 bg-white focus:outline-none appearance-none" v-model="glassFiltre">
+              <option>Verres</option>
+              <option v-for="glass in categories.glass" :key="glass">{{glass.strGlass}}</option>
+            </select>
+          </div>
+        </div>
+        <div class="searchButton">
+          <button class="bg-red-400 text-white text-xl rounded-full px-8 py-1 shadow" v-on:click="searchFilter">Rechercher</button>
+        </div>
+      </div>
     </div>
   </div>
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
@@ -49,8 +82,13 @@ export default {
   data() {
     return {
       searchText: "",
+      searchFilterText: "",
       results: null,
       FilterPage: false,
+      alcoholic: false,
+      categories: null,
+      categoryFiltre: "Catégories",
+      glassFiltre: "Verres"
     }
   },
   methods: {
@@ -71,12 +109,73 @@ export default {
         this.results = response.data.drinks;
       })
     },
+    searchFilter() {
+      let chainFilter = "";
+      
+      if (this.searchFilterText !== "") {
+        chainFilter += "?i=" + this.searchFilterText;
+      }
+
+      if (chainFilter === "") {
+        let alcoholic = this.alcoholic ? "?a=Alcoholic" : "?a=Non alcoholic";
+        chainFilter += alcoholic;
+      }else {
+        let alcoholic = this.alcoholic ? "&a=Alcoholic" : "&a=Non alcoholic";
+        chainFilter += alcoholic;
+      }
+
+      if (this.categoryFiltre !== "Catégories") {
+        if (chainFilter === "") {
+          chainFilter += "?c=" + this.categoryFiltre;
+        }else {
+          chainFilter += "&c=" + this.categoryFiltre;
+        }
+      }
+
+      if (this.glassFiltre !== "Verres") {
+        if (chainFilter === "") {
+          chainFilter += "?g=" + this.glassFiltre;
+        }else {
+          chainFilter += "&g=" + this.glassFiltre;
+        }
+      }
+
+      console.log(chainFilter);
+
+      axios.get('https://www.thecocktaildb.com/api/json/v2/9973533/filter.php' + chainFilter)
+      .then(response =>{
+        this.results = response.data.drinks;
+      })
+
+    }
+
 
   },
+  async created() {
+    await axios.get('https://www.thecocktaildb.com/api/json/v2/9973533/list.php?c=list')
+    .then(response => {
+      this.categories = response.data.drinks;
+    }),
+    await axios.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list')
+    .then(response => {
+      this.categories.glass = response.data.drinks;
+    })
+  }
 };
 </script>
 
 <style>
+  /* CHECKBOX TOGGLE SWITCH */
+/* @apply rules for documentation, these do not work as inline style */
+.toggle-checkbox:checked {
+  @apply: right-0 border-red-200;
+  right: 0;
+  border-color: #ffbcbc;
+}
+.toggle-checkbox:checked + .toggle-label {
+  @apply: bg-red-200;
+  background-color: #ffbcbc;
+}
 </style>
 
 <!--
